d832d4b0647b38507187c1a4d996e22c27a73790
markinivan <markinivan2004@yandex.ru>
2022-10-06 22:34:53 +0300

    исправлено деление 0 на -

--- TraningSolution/TraningProject_DivOperation/div_operation.c
+++ TraningSolution/TraningProject_DivOperation/div_operation.c
@@ -3,8 +3,13 @@ int main() {
 	int a, b;
 	float div;
 	scanf_s("%d %d", &a, &b);
-	div = a / b;
-	printf("%d", div);
+	if (b != 0 && a!=0) {
+		div = a / b;
+		printf("%f\n", div);
+	}else if(a==0 && b!=0){
+		div = 0;
+		printf("%f\n", div);
+	}
 	system("pause");
 	return 0;
-}
+}
